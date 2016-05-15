402fdfd3050d761fc87c95a7b294e596bb2d129e
Björn Kihlberg <hoppfull@gmail.com>
2016-05-15 17:43:01 +0200

    implemented MVVM ex02 example

---
+++ MVVM/ex02/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace ex02 {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVVM/ex02/ViewModels/MyViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ex02.Commands;
+
+namespace ex02.ViewModels {
+    public class MyViewModel {
+        public MyCommand PrintCommand { get; }
+        public MyViewModel() {
+            PrintCommand = new MyCommand(this);
+        }
+        public void MyMethod() {
+            Console.WriteLine("hello");
+        }
+    }
+}
