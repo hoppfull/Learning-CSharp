68c50f0755b7f6fef7411b80b3ff1a4668d9d525
Björn Kihlberg <hoppfull@gmail.com>
2016-05-15 16:57:46 +0200

    implemented MVVM ex01

--- MVVM/ex01/Models/MyModel.cs
+++ MVVM/ex01/Models/MyModel.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace ex01.Models {
     class MyModel : INotifyPropertyChanged {
+        private string myProperty;
+        public string MyProperty {
+            get { return myProperty; }
+            set {
+                myProperty = value;
+                OnPropertyChanged("MyProperty");
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
