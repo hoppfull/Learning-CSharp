0198c07e3ea7bb84595a95b7ba4fdfa66171969f
Björn Kihlberg <hoppfull@gmail.com>
2016-05-15 15:53:32 +0200

    implemented advanced ex10 example

---
+++ Advanced/ex10/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// https://msdn.microsoft.com/en-us/library/8627sbea.aspx
+namespace ex10 {
+    class Program {
+        static void Main(string[] args) {
+            MyClass myObj = new MyClass();
+            Action f = () => Console.WriteLine("hello");
+
+            myObj.MyAction += f; // cmd: "Function added!"
+            myObj.MyAction += f; // cmd: "Function added!"
+            myObj.MyAction += f; // cmd: "Function added!"
+            myObj.MyAction -= f; // cmd: "Function removed!"
+
+            // This is bad, the event keyword prevents this (encapsulation):
+            myObj.BadEvent = () => Console.WriteLine("noo");
+            myObj.BadEvent(); // cmd: "noo"
+
+            // This is better, we cannot mess with the field itself from here:
+            myObj.GoodEvent += () => Console.WriteLine("yay");
+            // And we can't call it directly which is a good thing!
+
+            myObj.MyMethod(); /*
+            cmd: hello
+            cmd: hello
+            cmd: yay */
+        }
+    }
+
+    class MyClass {
+        public Action BadEvent;
+
+        public event Action GoodEvent;
+
+        private Action myAction;
+        // We can explicitly specify what to do when adding and removing functions:
+        public event Action MyAction {
+            add {
+                Console.WriteLine("Function added!");
+                myAction += value;
+            }
+            remove {
+                Console.WriteLine("Function removed!");
+                myAction -= value;
+            }
+        }
+
+        public void MyMethod() {
+            // equivalent to myAction(); but doesn't execute if myAction == null:
+            myAction?.Invoke();
+            // equivalent to GoodEvent(); but doesn't execute if GoodEvent == null:
+            GoodEvent?.Invoke();
+        }
+    }
+}
---
+++ MVVM/ex01/Models/MyModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01.Models {
+    class MyModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
