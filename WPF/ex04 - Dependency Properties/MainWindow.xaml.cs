5cd187337844a5a93c0ecd0894bf8f068ea502fd
Björn Kihlberg <hoppfull@gmail.com>
2016-06-18 01:18:03 +0200

    implemented wpf example ex04 - Dependency Properties

---
+++ WPF/ex04 - Dependency Properties/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ex04 {
+    public partial class MainWindow : Window {
+        public string MyProperty { get; set; }
+        public MainWindow() {
+            InitializeComponent();
+
+            DataContext = this;
+
+            btn_myButton.Click += (o, e) => {
+                MessageBox.Show(MyProperty);
+            };
+        }
+    }
+}
---
+++ WPF/ex04 - Dependency Properties/MyUserControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ex04 {
+    public partial class MyUserControl : UserControl {
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string), typeof(MyUserControl));
+        public string Label {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(MyUserControl));
+        public string Text {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public MyUserControl() {
+            InitializeComponent();
+            (Content as FrameworkElement).DataContext = this;
+        }
+    }
+}
