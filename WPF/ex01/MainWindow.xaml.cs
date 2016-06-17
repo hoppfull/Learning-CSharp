68ed31828a03626a925ece4e156b76cb9d88965b
Björn Kihlberg <hoppfull@gmail.com>
2016-06-17 23:49:49 +0200

    added new set of WPF examples and implemented first two

---
+++ WPF/ex01/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows;
+
+namespace ex01 {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            Console.WriteLine("Check out XAML file!");
+        }
+    }
+}
