564547fb680d7d13c4edb7e14a8f71d35815c18b
Björn Kihlberg <hoppfull@gmail.com>
2016-06-17 23:56:55 +0200

    implemented WPF example ex03 - DataContext

---
+++ WPF/ex03 - DataContext/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace ex03 {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            DataContext = new MyClass { MyProperty = "Hello DataContext!" };
+        }
+    }
+
+    public class MyClass {
+        public string MyProperty { get; set; }
+    }
+}
